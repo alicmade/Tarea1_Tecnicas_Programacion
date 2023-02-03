83c21f1cc5b7493fb7cdbcbfcc84e18ac87322ba
alicmade <113613608+alicmade@users.noreply.github.com>
2023-02-03 10:30:27 +0100

    Update pregunta1.java

--- pregunta1.java
+++ pregunta1.java
@@ -1,26 +1,6 @@
 //¿Qué algoritmos usas en tu vida cotidiana?
 
 
-// Path: pregunta2.java
-//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
-    
-//¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
-
-//¿Qué algoritmos usas en tu vida cotidiana?
-
-
-// Path: pregunta4.java
-//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
-
-// Path: pregunta5.java
-
-
-// Path: pregunta6.java
-//¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
-
-// Path: pregunta7.java
-
-// Path: pregunta8.java
 
 class   pregunta9   { 
      public   static   void   main ( String []   args )   { 
