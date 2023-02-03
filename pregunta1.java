5da8cc946d1285c646032acf26b16911a84a0a88
alicmade <113613608+alicmade@users.noreply.github.com>
2023-02-03 10:31:53 +0100

    Update pregunta1.java

--- pregunta1.java
+++ pregunta1.java
@@ -1,13 +1,7 @@
-//¿Qué algoritmos usas en tu vida cotidiana?
+__¿Qué algoritmos usas en tu vida cotidiana?__
 
-¿Piensas que podrías escribir un programa para hacerlos más eficientes?
+jjj
 
+__¿Piensas que podrías escribir un programa para hacerlos más eficientes?__
 
-
-class   pregunta9   { 
-     public   static   void   main ( String []   args )   { 
-         System . out . println ( "¿Qué algoritmos usas en tu vida cotidiana?" ); 
-    
-     } 
- }
 
