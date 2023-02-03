418d0115aade6ca7da3caa61010e8be962b3df48
alicmade <113613608+alicmade@users.noreply.github.com>
2023-02-03 10:43:32 +0100

    Update pregunta2.java

--- pregunta2.java
+++ pregunta2.java
@@ -1,9 +1,5 @@
-//¿Piensas que podrías escribir un programa para hacerlos más eficientes?
-class   pregunta9   { 
-     public   static   void   main ( String []   args )   { 
-         System . out . println ( "¿Qué algoritmos usas en tu vida cotidiana?" ); 
-    
-     } 
- }
- 
+//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+// Por ejemplo algorítmos recursivos o  de secuencia una cosa lleva a la otra, si la realiza como es o sino vuelves a iniciar hasta que lo hagas bien,
+//vas pasando de niveles 
 
